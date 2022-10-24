5068cec5564b45dcdd4297e0c83b160261976170
Alicia Wilkerson <matalina@gmail.com>
2022-10-24 12:40:22 -0500

    predefined maps

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,25 @@
 <script lang="ts">
 import Calendar from './components/widgets/Calendar.svelte';
-import MapEncounters from "./components/generators/MapEncounters.svelte";
+import RandomEncounters from "./components/generators/RandomEncounters.svelte";
 import Oracle from "./components/generators/Oracle.svelte";
 import Today from "./components/widgets/Today.svelte";
 import RealmDate from './components/generators/RealmDate.svelte';
-    import Keywords from './components/generators/Keywords.svelte';
+import Keywords from './components/generators/Keywords.svelte';
+import Campaign from './components/generators/Campaign.svelte';
+import { onMount } from 'svelte';
 
+let tab = "random";
+
+function setTab(tabName: string) {
+  tab = tabName;
+  localStorage.setItem('tab', tab);
+}
+
+onMount(() => {
+  if(localStorage.getItem('tab')) {
+    tab = localStorage.getItem('tab');
+  }
+});
 </script>
 
 <main class="md:flex">
@@ -17,7 +31,19 @@ import RealmDate from './components/generators/RealmDate.svelte';
     <Calendar />
     <RealmDate />
   </div>
-  <MapEncounters />
+  <div class="md:w-3-4 flex flex-col">
+    <div class="ml-3 mt-3">
+      <button class="py-1 px-3 border mr-3 hover:bg-purple-100" on:click={() => setTab('random')}>Random</button>
+      <button class="py-1 px-3 border hover:bg-purple-100"  on:click={() => setTab('campaign')}>Campaign</button>
+    </div>
+    {#if tab === 'random'}
+    <RandomEncounters />
+    {/if}
+    {#if tab === 'campaign'}
+    <Campaign />
+    {/if}
+  </div>
+
 </main>
 
 
