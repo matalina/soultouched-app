b619b2cb6c2ff03eec53784d97307e38336e3bd0
matalina <matalina@gmail.com>
2023-05-02 12:08:26 +0000

    moved to single page

--- src/components/pages/Home.svelte
+++ src/components/pages/Home.svelte
@@ -4,13 +4,19 @@
   import DiceRoller from '../generators/DiceRoller.svelte';
   import RandomTables from '../generators/RandomTables.svelte';
   import CypherNpc from '../generators/cypher/CypherNPC.svelte';
+  import Tasks from '../generators/cypher/Tasks.svelte';
+  import Status from '../generators/cypher/Status.svelte';
+  import Npc from '../generators/NPC.svelte';
 </script>
 
 <main class="md:flex">
   <div class="mx-auto md:w-1/4">
     <Oracle />
     <DiceRoller />
+    <Tasks />
+    <Status />
     <CypherNpc />
+    <Npc />
     <RandomTables />
     <Keywords />
   </div>
