00925920257cb3b7bb441a93be63ff428c8865c2
Alicia Wilkerson <matalina@gmail.com>
2023-05-18 16:04:47 -0500

    adding links

--- src/App.svelte
+++ src/App.svelte
@@ -3,19 +3,21 @@
   import Home from './components/pages/Home.svelte';
   import Disclaimer from './components/ui/Disclaimer.svelte';
   import Character from './components/pages/Character.svelte';
+  import { SvelteComponentDev } from 'svelte/internal';
 
   export let url = '';
 </script>
 
-<Router url={url}>
-
+<Router {url}>
   <section>
-    <Route path="/" component={Home}/>
-    <Route path="/character" component={Character}/>
+    <Route path="/" component={Home} />
+    <Route path="/character" component={Character} />
   </section>
+  <nav>
+    <Link to="/">Home</Link> | <Link to="/character">Character Sheet</Link>
+  </nav>
   <Disclaimer />
 </Router>
 
 <style lang="postcss">
-
 </style>
